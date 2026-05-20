8459788c17d0acff76b5da529d0e8eb0609eb8bb
Kajahols <kajaholstrom@icloud.com>
2026-05-20 22:29:16 +0200

    fikset header

---
+++ pakksekk.js
@@ -0,0 +1,6 @@
+const start = document.querySelector("button")
+const oppdarg = document.getElementById("oppdrag")
+
+start.addEventListener("click", function() {
+    oppdarg.style.display = "block"
+})
