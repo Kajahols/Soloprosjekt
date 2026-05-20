3e43b6548d7c6113970d17a4b42069b21b16ad4c
Kajahols <kajaholstrom@icloud.com>
2026-05-20 22:43:45 +0200

    pakk js

--- pakksekk.js
+++ pakksekk.js
@@ -1,6 +1,10 @@
 const start = document.querySelector("button")
 const oppdarg = document.getElementById("oppdrag")
+const svar = document.querySelector(".pakk")
 
 start.addEventListener("click", function() {
     oppdarg.style.display = "block"
+    start.style.display = "none"
+    svar.style.display = "flex"
+
 })
