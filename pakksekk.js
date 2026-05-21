773ca6c902f541f24b318085b722e23abeb17de9
Kajahols <kajaholstrom@icloud.com>
2026-05-21 10:36:29 +0200

    la til pakkeliste

--- pakksekk.js
+++ pakksekk.js
@@ -1,6 +1,6 @@
 const start = document.querySelector("button")
 const oppdarg = document.getElementById("oppdrag")
-const svar = document.querySelector(".pakk")
+const svar = document.querySelector(".input-rad")
 
 start.addEventListener("click", function () {
     oppdarg.style.display = "block"
@@ -9,3 +9,24 @@ start.addEventListener("click", function () {
 
 })
 
+const inputElement = document.querySelector("input")
+const liste = document.querySelector(".pakk")
+const leggtil = document.getElementById("leggtil")
+
+let click = 0
+
+leggtil.addEventListener("click", function() {
+    const liElement = document.createElement("li")
+            
+    liElement.innerHTML = inputElement.value
+    liElement.onclick = function () {
+                liElement.classList.toggle("done")
+                click +=1
+
+            }
+
+            
+
+    liste.appendChild(liElement)
+    inputElement.value = ''
+})
