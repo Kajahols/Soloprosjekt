180c70690f804f30fd5f95d94179a2555b9beee9
Kajahols <kajaholstrom@icloud.com>
2026-05-21 12:45:36 +0200

    fikset burger

--- pakksekk.js
+++ pakksekk.js
@@ -30,3 +30,17 @@ leggtil.addEventListener("click", function() {
     liste.appendChild(liElement)
     inputElement.value = ''
 })
+
+const crossElm = document.querySelector(".kryss")
+const burgerElm = document.querySelector(".burger")
+const headerElm = document.querySelector("header")
+
+burgerElm.addEventListener("click", function () {
+    headerElm.classList.add("active")
+    burgerElm.style.display = "none"
+})
+
+crossElm.addEventListener("click", function () {
+    headerElm.classList.remove("active")
+    burgerElm.style.display = "block"
+})
